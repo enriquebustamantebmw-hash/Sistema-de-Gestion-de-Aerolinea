a7e7ac559701599c4c6843a35f59dd67e5a52f84
enrique <enriquebustamantebmw@gmail.com>
2026-04-09 17:05:23 -0300

    Primer commit se agregan aviones

---
+++ GestionAerolinea.py
@@ -0,0 +1,14 @@
+"""Sistema de Gestión de Aerolínea - Programación 1 - Grupo 4"""
+
+
+#aviones
+aviones_modelo     = ["A320", "A320", "A320", "A320", "A350", "Boeing 777"]
+aviones_tipo       = ["continental", "continental", "continental", "continental", "intercontinental", "intercontinental"]
+aviones_capacidad  = [180, 180, 180, 180, 300, 300]
+aviones_disponible = [True, True, True, True, True, True]
+
+#vuelos
+vuelos_origen  = []
+vuelos_destino = []
+vuelos_tipo    = []
+vuelos_avion   = []
